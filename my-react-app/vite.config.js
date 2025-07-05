ede5a5cc9b6a60549e66e3bf0364bdce088cb036
vardan.torosyan <vardan.torosyan@daniam.am>
2025-07-05 23:19:06 +0400

    vite update

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 
 export default defineConfig({
-  plugins: [ tailwindcss(),react()],
+  plugins: [ react()],
   base: process.env.VITE_BASE_PATH || "/Amclean"
 })
